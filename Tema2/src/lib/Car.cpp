cd7028433b4d230fcac2a4baf0899e0d5ba7b4d9
AndreeaLazaroniu <andreea.lazaroniu@student.upt.ro>
2023-11-16 00:25:24 +0200

    added subclass of Car class and some updates

--- Tema2/src/lib/Car.cpp
+++ Tema2/src/lib/Car.cpp
@@ -6,6 +6,7 @@ using namespace NCar;
 
 Car::Car(int year, char* brand, char* fuel){
     std::cout<<"I was created"<<std::endl;
+    
     this->year=year;
     this->brand=brand;
     this->fuel=fuel;
@@ -13,6 +14,7 @@ Car::Car(int year, char* brand, char* fuel){
 
 Car::Car(const Car &cr){
     std::cout<<"I was copied"<<std::endl;
+
     this->year=cr.year;
     this->brand = new char[strlen(cr.brand)+1];
     memcpy(this->brand, cr.brand, strlen(cr.brand)+1);
@@ -22,6 +24,7 @@ Car::Car(const Car &cr){
 
 Car::Car(Car &&prevCar){
     std::cout<<"I was moved"<<std::endl;
+    
     this->year=prevCar.year;
     this->brand=prevCar.brand;
     delete [] prevCar.brand;
@@ -30,19 +33,19 @@ Car::Car(Car &&prevCar){
 }
 
 Car::~Car(){
-    std::cout<<"I was deleted"<<std::endl;
+    std::cout << "I was deleted" << std::endl;
     delete [] this->brand;
     delete [] this->fuel;
 }
 
 void Car::numberWheels(){
-    std::cout<<"4"<<std::endl;
+    std::cout << "Number of Wheels: 4" << std::endl;
 }
 
 void Car::show(){
-    std::cout<<"brand:" <<this->brand<<std::endl;
+    std::cout <<"Brand:" << this->brand << "\nYear: " << this->year << "\nFuel: " << this->fuel << std::endl;
 }
 
 void Car::getYear(){
-    std::cout<<"Year"<<this->year<<std::endl;
+    std::cout << "Year: " << this->year << std::endl;
 }
--- Tema2/src/main.cpp
+++ Tema2/src/main.cpp
@@ -1,9 +1,11 @@
 # include <cstring>
 # include "lib/Car.hpp"
+# include "lib/SportCar.hpp"
 # include "lib/Vehicle.hpp"
 # include <iostream>
 
 using namespace NCar;
+using namespace NSportCar;
 
 int main(){
     char *brand = new char[50];
@@ -13,6 +15,14 @@ int main(){
     Car cr(2022, brand, fuel);
 
     cr.show();
+
+    char *brand1 = new char[50];
+    memcpy(brand1, "Dacia", 50);
+    char *fuel1 = new char[50];
+    memcpy(fuel1, "diesel", 50);
+    SportCar cr1(2023, brand1, fuel1, 150);
+
+    cr1.show();
 
     return 0;
 }
